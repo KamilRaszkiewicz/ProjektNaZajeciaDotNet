77410ffc1c197472ab4bdf62b505eb0aab87212e
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-05-20 22:48:49 +0200

    Implemented validation attribute FormImageAttribute

---
+++ Attributes/FormImageAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projekt.Attributes
+{
+    /// <summary>
+    /// Attribute used to validate form images binded to IFormFile properties
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class FormImageAttribute : ValidationAttribute
+    {
+        private readonly static string[] _allowedContentTypes =
+        {
+            "image/jpeg",
+            "image/png"
+        };
+
+        private readonly static int _maxBytesLength = 8 * 1024 * 1024;
+
+        public override bool IsValid(object? value)
+        {
+            if(value == null) return true;
+
+            var formImage = value as IFormFile;
+
+            if (formImage == null)
+            {
+                throw new InvalidOperationException("FormImageValidationAttribute can be used only on properties of type IFormFile");
+            }
+
+            if (!_allowedContentTypes.Contains(formImage.ContentType)) return false;
+
+            if (formImage.Length > _maxBytesLength) return false;
+
+            return true;
+        }
+    }
+}
