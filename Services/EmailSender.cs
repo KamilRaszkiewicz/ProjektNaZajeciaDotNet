5afa99f9b795f507ab8e9ca2bddf8d0e95ed6763
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-05-23 09:18:00 +0200

    Implemented IEmailSender service

---
+++ Services/EmailSender.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using Projekt.Options;
+using System.Net.Mail;
+
+namespace Projekt.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly SmtpClient _smtpClient;
+        private readonly string _senderEmail;
+
+        public EmailSender(IOptions<SmtpOptions> opts)
+        {
+            _senderEmail = opts.Value.SenderEmail;
+            _smtpClient = new SmtpClient("127.0.0.1", 25);
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            await _smtpClient.SendMailAsync(_senderEmail, email, subject, htmlMessage);
+        }
+    }
+}
+
