61dfd44aa18930e4e54f8e555832e7488ef2fb43
KamilRaszkiewicz <Kamil.Raszkiewicz02@gmail.com>
2023-05-24 22:39:10 +0200

    Added conditional try-catch block to allow app to work without smtp server during development

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -9,15 +9,26 @@ namespace Projekt.Services
     {
         private readonly SmtpClient _smtpClient;
         private readonly string _senderEmail;
+        private readonly bool _isDevelopment;
+        private readonly ILogger<EmailSender> _logger;
 
-        public EmailSender(IOptions<SmtpOptions> opts)
+        public EmailSender(ILogger<EmailSender> logger,IOptions<SmtpOptions> opts, IWebHostEnvironment env)
         {
+            _isDevelopment = env.IsDevelopment();
             _senderEmail = opts.Value.SenderEmail;
             _smtpClient = new SmtpClient(opts.Value.Host, opts.Value.Port);
+            _logger = logger;
         }
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            await _smtpClient.SendMailAsync(_senderEmail, email, subject, htmlMessage);
+            try
+            {
+                await _smtpClient.SendMailAsync(_senderEmail, email, subject, htmlMessage);
+            }
+            catch (SmtpException e) when (_isDevelopment)
+            {
+                _logger.LogInformation($"Exception thrown: {e.Message}");
+            }
         }
     }
 }
